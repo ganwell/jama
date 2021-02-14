7ab582ef42da5b49f79517bc79613bffe76b8bb9
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2021-02-14 18:09:20 +0100

    Make autotest faster

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ import os
 from hypothesis import settings
 
 settings.register_profile("default", max_examples=1000)
+settings.register_profile("auto", max_examples=20)
 profile = os.environ.get("JAMA_PROFILE", "default")
 settings.load_profile(profile)
