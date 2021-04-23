1969f84294816138d0bd29e2ec3142f3b6b9f91d
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2021-04-23 19:03:13 +0200

    cr: start state class

--- jama/change.py
+++ jama/change.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import IntEnum
 from typing import Any, Generator, Iterable, Union, cast
 
@@ -6,6 +8,8 @@ from attr import dataclass
 from pyrsistent import ny, pset, pvector
 from pyrsistent.typing import PSet, PVector
 from retworkx import PyDAG  # type: ignore
+
+Edge = tuple[int, int]
 
 
 class FileNodes(IntEnum):
@@ -22,7 +26,7 @@ class FileRepr(object):
 
     @classmethod
     def from_user_repr(cls, node_list):
-        return cls(node_list=[x + FileNodes.content for x in node_list])
+        return cls([x + FileNodes.content for x in node_list])
 
     def to_user_repr(self):
         return self.node_list.transform([ny], lambda x: x - FileNodes.content)
@@ -34,7 +38,18 @@ class FileReprEdit(FileRepr):
 
     @classmethod
     def from_size(cls, size):
-        return cls(pvector(range(size)), size - 1)
+        frepr = FileReprEdit.from_user_repr(pvector(range(size)))
+        return cls(frepr.node_list, size + FileNodes.content - 1)
+
+    @classmethod
+    def from_user_repr(cls, node_list):
+        max_uid = 0
+        node_new = []
+        for x in node_list:
+            x += FileNodes.content
+            max_uid = max(max_uid, x)
+            node_new.append(x)
+        return cls(node_new, max_uid)
 
     def __len__(self):
         return len(self.node_list)
@@ -57,3 +72,77 @@ class FileReprEdit(FileRepr):
             node_list[:offset] + node_list[offset + size :],
             self.max_uid,
         )
+
+
+# State is something like a CRDT
+@dataclass(slots=True, frozen=True)
+class State(object):
+    nodes: PVector[bool]
+    edges: PSet[Edge]
+    max_node: int
+    history: PVector[Change]
+
+    @staticmethod
+    def _node_list_to_edges(
+        nodes: Iterable[int],
+    ) -> Iterable[Edge]:
+        content = FileNodes.content
+        prev = FileNodes.start + content
+        for node in nodes:
+            yield (prev, node)
+            prev = node
+        yield (prev, FileNodes.end + content)
+
+    @classmethod
+    def from_graph(cls, nodes: Iterable[bool], edges: Iterable[Edge]):
+        # TODO add consistency check
+        nodes = pvector(nodes)
+        return cls(nodes, pset(edges), len(nodes) - 1, pvector())
+
+    @classmethod
+    def from_file(cls, file_: FileRepr):
+        node_list = file_.node_list
+        max_node = -1
+        if node_list:
+            max_node = max(node_list)
+            nodes = pvector([False] * (max_node + 1))
+        else:
+            nodes = pvector()
+
+        for i in range(FileNodes.content):
+            nodes = nodes.set(i, True)
+        for i in node_list:
+            nodes = nodes.set(i, True)
+        return cls(
+            nodes, pset(State._node_list_to_edges(node_list)), max_node, pvector()
+        )
+
+    def to_user_edges(self) -> Iterable[Edge]:
+        c = FileNodes.content
+        for e in self.edges:
+            yield (e[0] - c, e[1] - c)
+
+    def to_user_nodes(self) -> Iterable[bool]:
+        return self.nodes[: FileNodes.content]
+
+    # def to_file(self) -> FileRepr:
+    #     edges = collect_deleted_nodes(self.edges, self.nodes)
+    #     edges = get_outgoing(edges)
+    #     node_list = edges_to_node_list(edges)
+    #     return FileRepr(pvector(node_list))
+
+    # def has_conflict(self) -> bool:
+    #     raise NotImplementedError()
+
+    # def delete(self, change: Delete) -> State:
+    #     return State(
+    #         self.nodes.set(change.line, False),
+    #         self.edges,
+    #         self.max_node,
+    #         self.history.append(change),
+    #     )
+
+
+@dataclass(slots=True, frozen=True)
+class Change(object):
+    pass
--- tests/test_change.py
+++ tests/test_change.py
@@ -2,12 +2,41 @@ import jama.change as cmod
 
 
 def test_file_repr():
-    file = cmod.FileRepr.from_user_repr([0])
-    assert file.node_list == [cmod.FileNodes.content]
-    assert file.to_user_repr() == [0]
-    file = cmod.FileRepr.from_user_repr([0, 1])
-    assert file.node_list == [cmod.FileNodes.content, cmod.FileNodes.content + 1]
-    assert file.to_user_repr() == [0, 1]
-    file = cmod.FileRepr.from_user_repr([1])
-    assert file.node_list == [cmod.FileNodes.content + 1]
-    assert file.to_user_repr() == [1]
+    file_ = cmod.FileRepr.from_user_repr([0])
+    assert file_.node_list == [cmod.FileNodes.content]
+    assert file_.to_user_repr() == [0]
+    file_ = cmod.FileRepr.from_user_repr([0, 1])
+    assert file_.node_list == [cmod.FileNodes.content, cmod.FileNodes.content + 1]
+    assert file_.to_user_repr() == [0, 1]
+    file_ = cmod.FileRepr.from_user_repr([1])
+    assert file_.node_list == [cmod.FileNodes.content + 1]
+    assert file_.to_user_repr() == [1]
+
+
+def test_file_repr_edit():
+    file_ = cmod.FileReprEdit.from_user_repr([0])
+    assert file_.node_list == [cmod.FileNodes.content]
+    assert file_.max_uid == cmod.FileNodes.content
+    assert file_.max_uid == file_.node_list[0]
+    file_ = cmod.FileReprEdit.from_size(2)
+    assert file_.max_uid == file_.node_list[1]
+    assert file_.max_uid == cmod.FileNodes.content + 1
+
+
+def test_state_from_file():
+    fn = cmod.FileNodes
+    ct = fn.content
+    a = cmod.FileReprEdit.from_size(2)
+    b = cmod.State.from_file(a)
+    assert b.nodes == [True, True, True, True]
+    assert b.to_user_nodes() == [True, True]
+    assert b.edges == {
+        (fn.start + ct, 0 + ct),
+        (0 + ct, 1 + ct),
+        (1 + ct, fn.end + ct),
+    }
+    assert set(b.to_user_edges()) == {
+        (fn.start, 0),
+        (0, 1),
+        (1, fn.end),
+    }
